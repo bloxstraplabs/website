3bb9f827f533c390b75021ddf933b53827df92df
krampus <krampuscg@proton.me>
2024-12-05 19:18:30 +0200

    Added a /GameJoin endpoint for easy sharing of game join links

---
+++ BloxstrapWebsite/Controllers/GameJoinController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BloxstrapWebsite.Controllers
+{
+    public class GameJoinController : Controller
+    {
+        public IActionResult Index()
+        {
+            var placeId = HttpContext.Request.Query["placeId"].ToString();
+            var instanceId = HttpContext.Request.Query["gameInstanceId"].ToString();
+            var url = $"roblox://experiences/start?placeId={placeId}";
+
+            if (!string.IsNullOrEmpty(instanceId))
+            {
+                url += $"&gameInstanceId={instanceId}";
+            }
+
+            return Redirect(url);
+        }
+    }
+}
